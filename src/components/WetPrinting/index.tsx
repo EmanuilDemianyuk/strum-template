b6f4a5ae21d922fbc46eb970eacc7f97bc2f2153
EmanuilDemianyuk <120477589+EmanuilDemianyuk@users.noreply.github.com>
2023-11-22 13:36:40 +0200

    refactore footer

--- src/components/WetPrinting/index.tsx
+++ src/components/WetPrinting/index.tsx
@@ -1,7 +1,7 @@
 
 const WetPrinting = () => {
   return (
-    <div className="flex justify-center items-center py-8 border-t-[0.3px]">
+    <div className="flex justify-center items-center py-4 border-t-[0.3px] border-BrandYellow">
       <p className="text-white font-medium">©Strum.cv</p>
     </div>
   );
--- src/container/Footer/Footer.tsx
+++ src/container/Footer/Footer.tsx
@@ -1,38 +1,12 @@
-import { useEffect, useState } from 'react';
 import Description from "src/components/Description";
-import MainLogo from "src/components/MainLogo";
-import NavLink from "src/components/NavMenu/NavLink";
-import SocialMedia from "src/components/SocialMedia";
 import WetPrinting from "src/components/WetPrinting";
 
 
 const Footer = () => {
-  const [optionNavLink, setOptionNavLink] = useState<boolean>(true);
-
-  useEffect(() => {
-    window.addEventListener(
-      "resize",
-      () => {
-        window.innerWidth >= 960 
-        ? setOptionNavLink(true)
-        : setOptionNavLink(false)
-      },
-    );
-  });
-  
 
   return (
-    <footer className="bg-black text-white">
-      <div className="container p-4 py-8 flex flex-wrap justify-between lg:flex-nowrap">
-        <div>
-          <MainLogo/>
-          <Description/>
-          <SocialMedia/>
-        </div>
-        <div className="min-w-[10em] text-center py-4">
-          <NavLink reverse={optionNavLink}/>
-        </div>
-      </div>
+    <footer className="bg-black text-white pt-4 text-center border-t border-BrandYellow">
+      <Description/>
       <WetPrinting/>
     </footer>
   )
