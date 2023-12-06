ea21aa17f3856b680a8ab2971b5feb19545a09ef
Arina Zinievych <120561883+arina-zinievych@users.noreply.github.com>
2023-12-06 19:28:39 +0200

    change ServicesCard component

--- src/components/OurServices/ServicesCard/index.tsx
+++ src/components/OurServices/ServicesCard/index.tsx
@@ -5,13 +5,10 @@ const ServicesCard = ( { title, img }: SectionProps ): JSX.Element => {
   return (
     <Card
     shadow={false}
-    className="h-full w-full overflow-hidden"
+    className="h-[330px] w-full overflow-hidden bg-BrandYellow"
     >
      <CardHeader
-      floated={false}
-      shadow={false}
-      color="transparent"
-      className="m-0 rounded-none h-[200px]"
+      className="m-0 rounded-none h-[250px]"
       >
         <img
           className="w-full h-full object-cover transition-transform hover:scale-[1.2]"
@@ -19,10 +16,10 @@ const ServicesCard = ( { title, img }: SectionProps ): JSX.Element => {
           alt="image"
         />
       </CardHeader>
-    <CardBody className="bg-BrandYellow flex justify-center items-center">
+    <CardBody className="flex justify-center items-center">
       <Typography
         variant="h6"
-        className="font-bold text-black min-h-[50px] sm:min-h-[80px] text-center font-content"
+        className="font-bold text-black text-center font-content h-[55px] sm:h-[70px]"
       >
         {title}
       </Typography>
